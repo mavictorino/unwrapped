c7416f86b286c508bd2d896e5dca2f7f7f707af0
Mariana Victorino <victorino.marii@gmail.com>
2026-08-19 16:23:59 +0100

    chore: initialize React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Unrapped</h1>
+      <p>Find something they'll actually love!</p>
+    </main>
+  )
+}
+
+export default App
